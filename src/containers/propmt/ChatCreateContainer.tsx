a7b8fdd89dc2c2939d2faad56d7d73631fd148e6
sojeongHwang <wjdth01@gmail.com>
2024-07-22 15:29:32 +0900

    [API 추가] kakao 이미지 생성 api 연동 api 생성

--- src/containers/propmt/ChatCreateContainer.tsx
+++ src/containers/propmt/ChatCreateContainer.tsx
@@ -1,8 +1,29 @@
 "use client";
+import { useEffect } from "react";
 import styled from "./chatCreate.module.css";
 import { IoIosSend } from "react-icons/io";
 
 function ChatListContainer() {
+  const handleCreateImage = async () => {
+    try {
+      const res = await fetch("/api/image-creator", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt: "A photo of a cute tiny monster on the beach, daylight.",
+        }),
+      });
+
+      const data = await res.json();
+      console.log("??res", data);
+    } catch (exception) {
+      // @todo 에러 처리
+      console.log("?exception", exception);
+    }
+  };
+
   return (
     <div className={styled.wrap}>
       <div className={styled.input_wrap}>
@@ -10,7 +31,7 @@ function ChatListContainer() {
           className={styled.textarea}
           placeholder="명령어를 입력해주세요."
         ></textarea>
-        <button className={styled.send_button}>
+        <button onClick={handleCreateImage} className={styled.send_button}>
           <IoIosSend className={styled.send_icon} size={20} />
         </button>
       </div>
