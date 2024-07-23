8d3f51c147f70edae67e69746f3a56f3e6358609
sojeongHwang <wjdth01@gmail.com>
2024-07-23 15:16:55 +0900

    [코드 수정] IOS 확대현상을 막기 위한 viewport 추가 및 textarea style 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import Title from "@/components/Title";
 import Toast from "@/containers/alert/Toast";
@@ -8,6 +8,13 @@ import SWRConfigContext from "@/context/SWRConfigContext";
 export const metadata: Metadata = {
   title: "이미지 생성기",
   description: "일러스트 이미지 생성기 사이트 입니다.",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
 };
 
 export default function RootLayout({
