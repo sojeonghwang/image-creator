c8f981776136642ec96291f79fa55b7ba33e7538
sojeongHwang <wjdth01@gmail.com>
2024-08-02 00:39:35 +0900

    [리팩토링] fetxh 함수 내 공통 코드 hook 로 변경

--- src/containers/propmt/ChatCreateContainer.tsx
+++ src/containers/propmt/ChatCreateContainer.tsx
@@ -5,10 +5,31 @@ import chatStore from "@/hooks/store/chat";
 import alertStore from "@/hooks/store/alert";
 import { useState } from "react";
 import Loading from "@/components/common/Loading";
+import useApiRequest from "@/hooks/useApiRequest";
+
+const requestDefaultOption = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 function ChatListContainer() {
   const [enteredPrompt, setEnteredPrompt] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const { sendRequest: translatorSendRequest, loading: translatorLoading } =
+    useApiRequest<{
+      message: { result: { translatedText: string } };
+    }>();
+
+  const { sendRequest: imageCreatorSendRequest, loading: imageCreatorLoading } =
+    useApiRequest<{
+      id: string;
+      images: {
+        id: string;
+        image: string;
+      }[];
+    }>();
+
   const { addChat, selectedMessage } = chatStore();
   const { setToastMessage } = alertStore();
 
@@ -32,93 +53,32 @@ function ChatListContainer() {
     }
   };
 
-  // @description 이미지 수정이 잘 안되어서 사용 안함
-  const handleUpdateImage = async (translatedText: string) => {
-    try {
-      if (!enteredPrompt.length) {
-        return;
-      }
-
-      const imageBase64Src = getBase64ImageStr(`image_${selectedMessage?.id}`);
-      addChat({
-        text: enteredPrompt,
-        id: enteredPrompt,
-      });
-      setEnteredPrompt("");
-      const res = await fetch("/api/image-editor", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt: translatedText,
-          image: imageBase64Src,
-        }),
-      });
-
-      const { data } = await res.json();
-
-      if (!!data?.id) {
-        addChat({
-          image: data?.images?.[0]?.image ?? "",
-          id: data.id,
-        });
-      }
-    } catch (exception) {
-      setToastMessage("에러가 발생했습니다.");
-      console.error(`[handleUpdateImage] - ${exception}`);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const getBase64ImageStr = (imageId: string) => {
-    const imageElement = document.getElementById(imageId) as HTMLImageElement;
-    //type 방어
-    if (!imageElement) {
-      return;
-    }
-
-    return imageElement.src.replace("data:image/jpeg;base64,", "");
-  };
-
   const handleTranslate = async () => {
     try {
       if (!enteredPrompt.length) {
         return;
       }
 
-      setIsLoading(true);
-
-      const res = await fetch("/api/translator", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
+      const res = await translatorSendRequest(
+        "/api/translator",
+        requestDefaultOption,
+        {
           prompt: enteredPrompt,
-        }),
-      });
+        }
+      );
 
-      if (!res.ok) {
-        const { message } = await res.json();
-        setToastMessage(message ?? "에러가 발생했습니다.");
-        return;
-      }
-
-      const { data } = await res.json();
-
-      if (!!data?.message?.result?.translatedText) {
-        const { translatedText = undefined } = data?.message?.result;
+      if (res.isSuccess) {
+        const { translatedText = undefined } = res.data?.message.result ?? {};
         if (!translatedText) {
           setToastMessage("번역에 실패했습니다.");
           return;
         }
 
         handleCreateImage(translatedText);
+      } else {
+        setToastMessage(res.errorMessage ?? "에러가 발생했습니다.");
       }
     } catch (exception) {
-      setIsLoading(false);
       setToastMessage("에러가 발생했습니다.");
       console.error(`[handleTranslate] - ${exception}`);
     }
@@ -134,41 +94,36 @@ function ChatListContainer() {
         id: enteredPrompt,
       });
       setEnteredPrompt("");
-      const res = await fetch("/api/image-creator", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
+
+      const res = await imageCreatorSendRequest(
+        "/api/image-creator",
+        requestDefaultOption,
+        {
           prompt: translatedText,
-        }),
-      });
+        }
+      );
 
-      if (!res.ok) {
-        const { message } = await res.json();
-        setToastMessage(message ?? "에러가 발생했습니다.");
-        return;
-      }
-
-      const { data } = await res.json();
-
-      if (!!data?.id) {
-        addChat({
-          image: data?.images?.[0]?.image ?? "",
-          id: data.id,
-        });
+      if (res.isSuccess) {
+        if (!!res.data?.id) {
+          addChat({
+            image: res.data.images[0].image ?? "",
+            id: res.data.id,
+          });
+        } else {
+          setToastMessage("이미지 생성에 실패했습니다.");
+        }
+      } else {
+        setToastMessage(res.errorMessage ?? "에러가 발생했습니다.");
       }
     } catch (exception) {
       setToastMessage("에러가 발생했습니다.");
       console.error(`[handleCreateImage] - ${exception}`);
-    } finally {
-      setIsLoading(false);
     }
   };
 
   return (
     <div className={styled.wrap}>
-      {isLoading ? (
+      {translatorLoading || imageCreatorLoading ? (
         <Loading />
       ) : (
         <div className={styled.input_wrap}>
